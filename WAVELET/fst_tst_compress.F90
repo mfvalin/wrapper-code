db89e6ffebe4d697acf9a3974e7a582e167002fc
Michel Valin <mfvalin@gmail.com>
2016-03-14 19:37:50 -0400

    new scores routine

--- WAVELET/fst_tst_compress.F90
+++ WAVELET/fst_tst_compress.F90
@@ -50,7 +50,7 @@ program test_compress
         call test_quantizing(z,ni,nj,nomvar,1)
       endif
       if(nomvar .ne. oldnam) then
-        if(oldnam .ne. '    ') write(0,*)'NAME=',oldnam,' levels=',ilev
+!         if(oldnam .ne. '    ') write(0,*)'NAME=',oldnam,' levels=',ilev
         oldnam = nomvar
         ilev = 0
       endif
@@ -66,11 +66,110 @@ program test_compress
   stop
 end program test_compress
 
+#define NSCORES 16
+
+subroutine scores(s,fa,fb,ni,nj,toler,diag,msg)
+  use ISO_C_BINDING
+  implicit none
+  integer, intent(IN) :: ni, nj
+  real, dimension(NSCORES), intent(OUT) :: s
+  real, dimension(ni,nj) :: fa, fb
+  real, intent(IN) :: toler             ! tolerance
+  logical, intent(IN) :: diag
+  character(len=*), intent(IN) :: msg
+
+  real *8 :: suma, sumb, sumab, suma2, sumb2, bias, rms, abserr, errmax, errrel
+  real *8 :: errrelmax, errrelavg, avga, avgb, stda, stdb, sd, corr, slope, intercept
+  integer :: i, j, n
+  real :: nonzero, fmin, fmax, span
+
+  s = 0.0
+  call s1scor(s(1),fa,fb,ni,nj,1,1,ni,nj,1)
+  suma = 0.0
+  sumb = 0.0
+  sumab = 0.0
+  suma2 = 0.0
+  sumb2 = 0.0
+  bias = 0.0
+  abserr = 0.0
+  rms = 0.0
+  errmax = 0.0
+  errrel = 0.0
+  errrelavg = 0.0
+  nonzero = 0
+  sd = 0       ! significant digits
+  n = ni*nj
+  corr = 0.0
+  slope = 0.0
+  intercept = 0.0
+  fmin = fa(1,1)
+  fmax = fmin
+  do j = 1 , nj
+  do i = 1 , ni
+    suma = suma + fa(i,j)
+    suma2 = suma2 + fa(i,j)*fa(i,j)
+    sumb = sumb + fb(i,j)
+    sumb2 = sumb2 + fb(i,j)*fb(i,j)
+    sumab = sumab + fa(i,j)*fb(i,j)
+    rms = rms + (fa(i,j)-fb(i,j)) * (fa(i,j)-fb(i,j))
+    abserr = abserr + abs(fa(i,j)-fb(i,j))
+    bias = bias +  (fb(i,j)-fa(i,j))
+    errmax = max( errmax,  abs(fa(i,j)-fb(i,j)) )
+    if( fa(i,j)*fb(i,j) .ne. 0) then
+      nonzero = nonzero + 1
+      errrel = abs(fa(i,j)-fb(i,j)) / abs(fa(i,j))
+      errrel = max(errrel, .0000005)
+      sd = sd - log10(errrel)
+      errrelavg = errrelavg + errrel
+      errrelmax = max( errrelmax , errrel )
+    endif
+    fmin = min(fmin,fa(i,j))
+    fmax = max(fmax,fa(i,j))
+  enddo
+  enddo
+  avga = suma / (ni*nj)
+  stda = sqrt( max( 0.0 , (suma2/n) - ((suma/n)*(suma/n)) ) )
+  avgb = sumb / (ni*nj)
+  stdb = sqrt( max( 0.0 , (sumb2/n) - ((sumb/n)*(sumb/n)) ) )
+  rms = sqrt( rms / (ni*nj) )
+  bias = bias / (ni*nj)
+  abserr = abserr / (ni*nj)
+  if(nonzero > 0) then
+    sd = sd / nonzero
+    errrelavg = errrelavg / nonzero
+  endif
+  span = min( abs(fmax-avga) , abs(avga-fmin) )
+  span = (fmax - fmin) / span
+  corr = (sumab - n*avga*avgb) / (n*stda*stdb)
+  slope = (sumab/n - avga*avgb) / ( suma2/n - avga*avga)
+  intercept = avgb - slope * avga
+  s(2) = rms           ! average squared error
+  s(3) = bias          ! bias
+  s(4) = abserr        ! average absolute error
+  s(5) = errmax        ! max absolute error
+  s(6) = errrelmax     ! max realtive error
+  s(7) = errrelavg     ! average relative error
+  s(8) = stda
+  s(9) = stdb
+  s(10) = sd
+  s(11) = corr
+
+  if(diag) then
+    print 100,msg,' S1=',s(1),' Bias=',s(3),' RMS=',s(2),' B/R=',nint(100*abs(s(3)/s(2))), &
+             ' Ae=',s(5),s(4),' Re=',s(6),s(7),' NZ=',nint(nonzero/(ni*nj)*100.0), &
+             ' dSTD=',stda-stdb,' SD=',sd,' dC1=',1.0-corr,' Sp=',nint(span*.5), &
+             ' A,B=',slope-1.0,intercept
+100 format(A,A,E8.2,A,E10.3,A,E9.3,A,I3,1H%,A,2E10.3,A,2E10.3,A,I3,1H%,A,E10.3,A,F4.1,A,E10.4,A,I5,A,E10.3,E11.3)
+  endif
+
+  return
+end subroutine scores
+
 subroutine test_quantizing(z,ni,nj,nomvar,mode)
   use ISO_C_BINDING
   implicit none
   integer, intent(IN) :: ni,nj
-  real, dimension(ni,nj), intent(IN), target :: z
+  real, dimension(ni,nj), intent(IN) :: z
   character(len=4), intent(IN) :: nomvar
   integer, intent(IN) :: mode
 
@@ -84,6 +183,7 @@ subroutine test_quantizing(z,ni,nj,nomvar,mode)
   integer :: mask
   real, dimension(ni,nj), target :: zz
   integer :: izz, maxexp1, maxexp2, maxexp
+  real, dimension(NSCORES) :: s
 
 !   if(mode .ne. 0) return
   zmin = z(1,1)
@@ -166,7 +266,8 @@ subroutine test_quantizing(z,ni,nj,nomvar,mode)
       if(coded==0) population(8) = population(8)+1
       if(mode == 0) znew = coded*rr + zmin
       if(mode .ne. 0) then
-        izz = transfer(z(i,j),izz)
+        zz(i,j) = z(i,j) * 1.000177
+        izz = transfer(zz(i,j),izz)
         izz = iand(izz,mask)
         ifactor = iand(ishft(izz,-23),Z'000000FF')
         if(ifactor < maxexp-31) then
@@ -196,12 +297,13 @@ subroutine test_quantizing(z,ni,nj,nomvar,mode)
     span = max(min(zmax-meanval,meanval-zmin),rr)
     span = rrange/span
 !     return
-    call s1scor(s1,z,zz,ni,nj,1,1,ni,nj,1)
+!     call s1scor(s1,z,zz,ni,nj,1,1,ni,nj,1)
     if(mode == 0)print 101,nomvar,population(8),population(0)-population(8),population(1:7)
     if(mode .ne. 0 .and. nint(span) < 32) return
-    print 100, nbits, toler*1.2 >= errmax, toler >= rms, toler,errmax,  bias,rms,relrms,rrange,s1,   zmax,zmin,meanval, &
-       nint(errmax/toler*100)*1.0, nint(rms/toler*100)*1.0, nint(bias/toler*1000000)*.0001, abs(bias/rms*100.0), &
-       (power-imax),nint(span),'  |'
+    call scores(s,z,zz,ni,nj,toler,.true.,'    ')
+!     print 100, nbits, toler*1.2 >= errmax, toler >= rms, toler,errmax,  bias,rms,relrms,rrange,s1,   zmax,zmin,meanval, &
+!        nint(errmax/toler*100)*1.0, nint(rms/toler*100)*1.0, nint(bias/toler*1000000)*.0001, abs(bias/rms*100.0), &
+!        (power-imax),nint(span),'  |'
 100 format(I4,2X,2L1, 2G12.4,2H |, 5G12.4,2H |, 3G12.4,2H | ,4F9.2,2H | 2I6,A)
 101 format(A,2X,9I8)
     if(imax > power - 2) then
