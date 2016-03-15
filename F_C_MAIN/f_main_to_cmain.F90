37ddf04942636ffb67435af24cc8eaaab825492d
Michel Valin <mfvalin@gmail.com>
2016-03-15 14:00:51 -0400

    setup for robodoc

--- F_C_MAIN/f_main_to_cmain.F90
+++ F_C_MAIN/f_main_to_cmain.F90
@@ -1,14 +1,15 @@
 #if defined(NEVER_TO_BE_TRUE_OR_ELSE)
-#
-# fake fortran main program for use with C programs that call fortran routines that
-# might require fortran runtime library initialization
-#
-# said main program may also be included in a static (.a) library
-# this has been tested with gfortran, Intel fortran, Portland group fortran
-#
-! ==================================================================================
-# simple example
+!****P* FMAIN/f_main_to_c_main
+! AUTHOR
+!   M.Valin Recherche en Prevision Numerique 2015/2016
+! SYNOPSIS
+  fake fortran main program for use with C programs that call fortran routines that
+  might require fortran runtime library initialization
 
+  said main program may also be included in a static (.a) library
+  this has been tested with gfortran, Intel fortran, Portland group fortran
+ 
+================================ simple example ================================
 cat >mydemo.c <<EOT
 #include <stdio.h>
 //int UseFmain();
@@ -24,17 +25,14 @@ EOT
 # straight
 s.cc -c -Dmain=MY_C_MAIN mydemo.c
 s.f90 f_main_to_cmain.F90 mydemo.o
-
-# with library version of f_main_to_cmain
+# or with library version of f_main_to_cmain
 s.f90 -c f_main_to_cmain.F90
 ar rcv libfmain.a f_main_to_cmain.o
 s.cc -c -Dmain=MY_C_MAIN mydemo.c
 s.f90 -o mydemo mydemo.o -L. -lfmain
 ./mydemo 1 2 3
 
-! ==================================================================================
-# example with tcl interpreter
-
+================================ example with tcl interpreter ================================
 cat >mytcl.c <<EOT
 #include <tcl.h>
 //int UseFmain();
@@ -58,14 +56,14 @@ int Tcl_AppInit(interp)
 EOT
 # Ubuntu 14.04 for the following lines
 s.cc -c -Dmain=MY_C_MAIN mytcl.c -I/usr/include/tcl8.5
-#
+# then
 s.f90  -o mytcl f_main_to_cmain.F90 mytcl.o -L/usr/lib/x86_64-linux-gnu -ltcl8.5
 # or
 s.f90 -c f_main_to_cmain.F90
 ar rcv libfmain.a f_main_to_cmain.o
 s.f90 -o mytcl mytcl.o -L. -lfmain -L/usr/lib/x86_64-linux-gnu -ltcl8.5
-! ==================================================================================
-# example for wish interpreter
+
+================================ example for wish interpreter ================================
 cat >mywish.c <<EOT
 #include <tcl.h>
 #include <tk.h>
@@ -95,15 +93,14 @@ int Tcl_AppInit(interp)
 EOT
 # Ubuntu 14.04 for the following lines
 s.cc -c -Dmain=MY_C_MAIN mywish.c -I/usr/include/tcl8.5
-#
+# then
 s.f90  -o mywish f_main_to_cmain.F90 mywish.o -L/usr/lib/x86_64-linux-gnu -ltcl8.5
 # or
 s.f90 -c f_main_to_cmain.F90
 ar rcv libfmain.a f_main_to_cmain.o
 s.f90 -o mywish mywish.o -L. -lfmain -L/usr/lib/x86_64-linux-gnu -ltcl8.5 -ltk8.5
-! ==================================================================================
-# example for python interpreter
 
+================================ example for python interpreter ================================
 cat >mypython.c <<EOT
 //int UseFmain();
 main(int argc, char** argv)
@@ -122,7 +119,8 @@ s.f90 -o mypython  f_main_to_cmain.F90 mypython.o -lpython2.7
 s.f90 -c f_main_to_cmain.F90
 ar rcv libfmain.a f_main_to_cmain.o
 s.f90 -o mypython  mypython.o -L. -lfmain -lpython2.7
-! ==================================================================================
+==================================================================================
+!******
 #endif
 program fcmain
   use ISO_C_BINDING
