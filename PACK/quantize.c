a1b01eea832352f130952ad4b11f4708fd80f67d
M. Valin <mfvalin@gmail.com>
2020-04-15 19:45:27 -0400

    added license

--- PACK/quantize.c
+++ PACK/quantize.c
@@ -1,3 +1,21 @@
+/* Hopefully useful routines for C and FORTRAN
+ * Copyright (C) 2020  Recherche en Prevision Numerique
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation,
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 typedef struct {
   int o;    // offset
