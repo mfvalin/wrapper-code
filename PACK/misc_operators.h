af891934891b087b60194977d0907da5d6b0fe75
M. Valin <mfvalin@gmail.com>
2022-07-22 10:30:48 -0400

    added mask generators

--- PACK/misc_operators.h
+++ PACK/misc_operators.h
@@ -20,4 +20,15 @@
 #define ABS(val)  ((val) < 0) ? (-(val)) : (val)
 #define MINMAX(min,max,src,n)  { int i=1 ; min = max = src[0] ; while(i++ < n) { min = MIN(min,src[i]) ; max = MAX(max,src[i]); } }
 
+// number of bits needed to represent range
+#define NEEDBITS(range,needed) { uint64_t rng = (range) ; needed = 1; while (rng >>= 1) needed++ ; }
+
+// 32 and 64 bit left aligned masks
+#define LMASK32(nbits)  ((~0)   << (32-nbits))
+#define LMASK64(nbits)  ((~0l)  << (64-nbits))
+
+// 32 and 64 bit left aligned masks
+#define RMASK32(nbits)  (~((~0)  << nbits))
+#define RMASK64(nbits)  (~((~0l) << nbits))
+
 #endif
