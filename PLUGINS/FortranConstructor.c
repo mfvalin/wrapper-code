0d70bca10fabf15dbff8035ba8837ee8308a752c
M. Valin <mfvalin@gmail.com>
2022-08-23 09:04:55 -0400

    comments

--- PLUGINS/FortranConstructor.c
+++ PLUGINS/FortranConstructor.c
@@ -4,5 +4,5 @@ void fortran_constructor() ;
 
 void __attribute__ ((constructor)) FortranConstructor(void) {
    printf("FortranConstructor calling fortran_constructor\n");
-   fortran_constructor() ;  // call library Fortran constructor
+   fortran_constructor() ;  // call user supplied Fortran constructor for library
 }
