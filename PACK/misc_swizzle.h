689b31060fc9b9056837004da2cb9340c4e2852d
M. Valin <mfvalin@gmail.com>
2022-09-12 10:17:17 -0400

    added restrict attributes, minor changes

--- PACK/misc_swizzle.h
+++ PACK/misc_swizzle.h
@@ -117,11 +117,11 @@ static int b_const = 0b01010101 ;
 static int index[8] = {0, 2, 4, 6, 1, 3, 5, 7 } ;
 
 // split a into even and odd terms (8 items)
-STATIC inline void SplitEvenOdd_8(void *pa, void *pe, void *po){
+STATIC inline void SplitEvenOdd_8(void * restrict pa, void * restrict pe, void * restrict po){
   int i, j ;
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
 #if defined(__x86_64__) && defined(__AVX2__) && defined(WITH_SIMD)
   __m256 v ;
   __m256i vx ;
@@ -131,7 +131,6 @@ STATIC inline void SplitEvenOdd_8(void *pa, void *pe, void *po){
   v = _mm256_permutevar8x32_ps(v, vx) ;
   _mm_storeu_ps(e, _mm256_extractf128_ps(v, 0) ) ;
   _mm_storeu_ps(o, _mm256_extractf128_ps(v, 1) ) ;
-// printf("+++\n");
 #else
   for(i=0, j=0 ; j<4 ; j++, i+=2){
     e[j] = a[i] ;
@@ -141,11 +140,11 @@ STATIC inline void SplitEvenOdd_8(void *pa, void *pe, void *po){
 }
 
 // split a into even and odd terms (16 items)
-STATIC inline void SplitEvenOdd_16(void *pa, void *pe, void *po){
+STATIC inline void SplitEvenOdd_16(void * restrict pa, void * restrict pe, void * restrict po){
   int i, j ;
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
 #if defined(__x86_64__) && defined(__AVX2__) && defined(WITH_SIMD)
   SplitEvenOdd_8(a  , e  , o  ) ;
   SplitEvenOdd_8(a+8, e+4, o+4) ;
@@ -158,11 +157,11 @@ STATIC inline void SplitEvenOdd_16(void *pa, void *pe, void *po){
 }
 
 // split a into even and odd terms (32 items)
-STATIC inline void SplitEvenOdd_32(void *pa, void *pe, void *po){
+STATIC inline void SplitEvenOdd_32(void * restrict pa, void * restrict pe, void * restrict po){
   int i, j ;
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
 #if defined(__x86_64__) && defined(__AVX2__) && defined(WITH_SIMD)
   SplitEvenOdd_16(a   , e  , o  ) ;
   SplitEvenOdd_16(a+16, e+8, o+8) ;
@@ -175,11 +174,11 @@ STATIC inline void SplitEvenOdd_32(void *pa, void *pe, void *po){
 }
 
 // split a into even and odd terms (64 items)
-STATIC inline void SplitEvenOdd_64(void *pa, void *pe, void *po){
+STATIC inline void SplitEvenOdd_64(void * restrict pa, void * restrict pe, void * restrict po){
   int i, j ;
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
 #if defined(__x86_64__) && defined(__AVX2__) && defined(WITH_SIMD)
   SplitEvenOdd_32(a   , e   , o   ) ;
   SplitEvenOdd_32(a+32, e+16, o+16) ;
@@ -193,44 +192,44 @@ STATIC inline void SplitEvenOdd_64(void *pa, void *pe, void *po){
 
 // split a into even and odd terms (n items)
 // if n is odd, n-1 is used
-STATIC inline void SplitEvenOdd_n(void *pa, void *pe, void *po, int n){
+STATIC inline void SplitEvenOdd_n(void * restrict pa, void * restrict pe, void * restrict po, int n){
   int i, j ;
   int nodd = n >> 1 ;
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
 
-  while(n >= 64){
+  while(n > 63){
     SplitEvenOdd_64(a , e , o ) ;
     a += 64 ; e += 32 ; o += 32 ; n -= 64 ;
   }
-  if(n >= 32){
+  if(n > 31){
     SplitEvenOdd_32(a , e , o ) ;
     a += 32 ; e += 16 ; o += 16 ; n -= 32 ;
   }
-  if(n >= 16){
+  if(n > 15){
     SplitEvenOdd_16(a , e , o ) ;
     a += 16 ; e += 8 ; o += 8 ; n -= 16 ;
   }
-  if(n >= 8){
+  if(n > 7){
     SplitEvenOdd_8(a , e , o ) ;
     a += 8 ; e += 4 ; o += 4 ; n -= 8 ;
   }
-  if(n < 8 && n > 0){
-    for(i=0, j=0 ; i<(n-1) ; j++, i+=2){
-      e[j] = a[i] ;
-      o[j] = a[i+1] ;
-    }
-    if(i<n) e[j] = a[i] ;  // n was odd ?
+//   if(n < 8 && n > 0){
+  for(i=0, j=0 ; j<(nodd & 0x3) ; j++, i+=2){
+    e[j] = a[i] ;
+    o[j] = a[i+1] ;
   }
+  if(n&1) e[j] = a[i] ;  // n was odd ?
+//   }
   return ;
 }
 
 // interleave even and odd terms into a (8 items)
 STATIC inline void ShuffleEvenOdd_8(void *pa, void *pe, void *po){
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
 #if defined(__x86_64__) && defined(__AVX__) && defined(WITH_SIMD)
   __m128 ve, vo ;
   ve = _mm_loadu_ps(e) ;
@@ -248,9 +247,9 @@ STATIC inline void ShuffleEvenOdd_8(void *pa, void *pe, void *po){
 
 // interleave even and odd terms into a (16 items)
 STATIC inline void ShuffleEvenOdd_16(void *pa, void *pe, void *po){
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
 #if defined(__x86_64__) && defined(__AVX__) && defined(WITH_SIMD)
   __m256 ve, vo, v0, v1 ;
   ve = _mm256_loadu_ps(e) ;
@@ -270,9 +269,9 @@ STATIC inline void ShuffleEvenOdd_16(void *pa, void *pe, void *po){
 
 // interleave even and odd terms into a (32 items)
 STATIC inline void ShuffleEvenOdd_32(void *pa, void *pe, void *po){
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
 #if defined(__x86_64__) && defined(__AVX__) && defined(WITH_SIMD)
   __m256 ve0, ve1, vo0, vo1, v0, v1, v2, v3 ;
   ve0 = _mm256_loadu_ps(e) ;
@@ -298,9 +297,9 @@ STATIC inline void ShuffleEvenOdd_32(void *pa, void *pe, void *po){
 
 // interleave even and odd terms into a (64 items)
 STATIC inline void ShuffleEvenOdd_64(void *pa, void *pe, void *po){
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
 #if defined(__x86_64__) && defined(__AVX__) && defined(WITH_SIMD)
   ShuffleEvenOdd_32(a   , e   , o   ) ;   // first 32
   ShuffleEvenOdd_32(a+32, e+16, o+16) ;   // next 32
@@ -318,37 +317,31 @@ STATIC inline void ShuffleEvenOdd_64(void *pa, void *pe, void *po){
 STATIC inline void ShuffleEvenOdd_n(void *pa, void *pe, void *po, int n){
   int i, j ;
   int nodd = n >> 1;
-  float *a = (float *) pa ;
-  float *e = (float *) pe ;
-  float *o = (float *) po ;
-#if defined(__x86_64__) && defined(__AVX__) && defined(WITH_SIMD)
-  while(n >= 64){
+  float * restrict a = (float *) pa ;
+  float * restrict e = (float *) pe ;
+  float * restrict o = (float *) po ;
+
+  while(n > 63){
     ShuffleEvenOdd_64(a   , e   , o   ) ;   // batch of 64
     n -= 64 ; a += 64 ; e += 32 ; o += 32 ;
   }
-  if(n >= 32){
+  if(n > 31){
     ShuffleEvenOdd_32(a, e, o) ;   // next 32
     n -= 32 ; a += 32 ; e += 16 ; o += 16 ;
   }
-  if(n >= 16){
+  if(n >= 15){
     ShuffleEvenOdd_16(a, e, o) ;   // next 32
     n -= 16 ; a += 16 ; e += 8 ; o += 8 ;
   }
-  if(n >= 8){
+  if(n > 7){
     ShuffleEvenOdd_8(a, e, o) ;   // next 32
     n -= 8 ; a += 8 ; e += 4 ; o += 4 ;
   }
-  for(i=0, j=0 ; j<nodd ; j++, i+=2){
+  for(i=0, j=0 ; j<(nodd & 0x3) ; j++, i+=2){
     a[i]   = e[j] ;
     a[i+1] = o[j] ;
   }
-#else
-  for(i=0, j=0 ; j<nodd ; j++, i+=2){
-    a[i]   = e[j] ;
-    a[i+1] = o[j] ;
-  }
-#endif
-  if(n&1) a[n-1] = e[nodd] ;   // odd number of values, one extra even value
+  if(n&1) a[i] = e[j] ;   // odd number of values, one extra even value
 }
 
 #endif
