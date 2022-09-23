b805c7882e097e5d81f5699e06bf1885d247210e
M. Valin <mfvalin@gmail.com>
2022-09-23 08:44:16 -0400

    added entropy and bits needed functions

--- PACK/misc_operators.c
+++ PACK/misc_operators.c
@@ -14,3 +14,73 @@
 
 #define STATIC extern
 #include <misc_operators.h>
+
+#include <math.h>
+#include <stdlib.h>
+
+void BitEntropy4(float entropy[4], uint32_t *bitstream, int npts, int nbits, int rshift) {
+  uint32_t bins[4][16] ;
+  uint32_t mask = 0xF ;
+  int i, j ;
+  uint32_t t ;
+  float e[4] ;
+  float prob, scale, ovlog2 ;
+
+  scale = 1.0f / (float)(npts) ;
+  ovlog2 = 1.0f / logf(2.0f) ;
+  for(j=0 ; j<4 ; j++) for(i=0 ; i<16 ; i++) bins[j][i] = 0 ;
+  for(j=0 ; j<4 ; j++) e[j] = 0.0f ;
+  for (i = 0; i < npts; i++) {
+    t = bitstream[i] ;
+    bins[0][t & mask]++ ; t >>= 4 ;
+    bins[1][t & mask]++ ; t >>= 4 ;
+    bins[2][t & mask]++ ; t >>= 4 ;
+    bins[3][t & mask]++ ;
+  }
+  for(j=0 ; j<4 ; j++){
+    for(i=0 ; i<16 ; i++){
+      if (bins[j][i] != 0) {
+        prob = (float)bins[j][i] * scale ;
+        e[j] += (prob * logf(prob) * ovlog2) ;
+      }
+    }
+  }
+  for(j=0 ; j<4 ; j++) entropy[j] = (-e[j]) ;
+}
+
+float BitEntropy(int32_t *bitstream, int npts, int nbits, int rshift) {
+    int lbin, sizebins;
+    int imin, imax, range, nbits_local ;
+    float prob, entropie, scale, ovlog2 ;
+    uint32_t *bins;
+    uint32_t mask ;
+    uint32_t samples = 0 ;
+
+    entropie = 0.0f;
+    mask = RMASK32(nbits) ;
+    // printf("imin : %d imax : %d range: %d, nbits : %d\n", imin, imax, imax-imin, nbits_local);
+
+    sizebins = 1 << (nbits-rshift) ;
+    bins = (uint32_t *) calloc(sizebins,sizeof(uint32_t));
+
+    for (int i = 0; i < npts; i++) {
+        lbin = (bitstream[i] & mask) >> rshift ;
+        bins[lbin]++;
+    }
+
+    scale = 1.0f / (float)(npts) ;
+    ovlog2 = 1.0f / logf(2.0f) ;
+    for (int i = 0; i < sizebins; i++) {
+        if (bins[i] != 0) {
+            prob = (float)(bins[i]) * scale ;
+            // printf("i: %d count: %d prob: %f contrib : %f \n", i, bins[i], prob, (prob * log(prob)/log(2.0)));
+            entropie += prob * logf(prob) * ovlog2 ;
+            samples++ ;
+        }
+    }
+
+    free(bins) ;
+// printf("sizebins = %d, mask = %8.8x, rshift = %d, samples = %d, entropie = %g\n", sizebins, mask, rshift, samples, entropie);
+    return -entropie ;
+}
+
--- PACK/misc_operators.h
+++ PACK/misc_operators.h
@@ -1,16 +1,18 @@
-// Hopefully useful code for C
-// Copyright (C) 2022  Recherche en Prevision Numerique
-//
-// This code is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation,
-// version 2.1 of the License.
-//
-// This code is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Library General Public License for more details.
-//
+/*
+Hopefully useful code for C
+Copyright (C) 2022  Recherche en Prevision Numerique
+
+This code is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation,
+version 2.1 of the License.
+
+This code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+*/
+#if ! defined(IN_FORTRAN_CODE) && ! defined(__GFORTRAN__)
 
 #if ! defined(MISC_OPERATORS)
 #include <stdint.h>
@@ -30,14 +32,14 @@
   while(i++ < n) { min = MIN(min,src[i]) ; max = MAX(max,src[i]); } \
 }
 
-// number of bits needed to represent range
+// number of bits needed to represent range (positive number)
 #define NEEDBITS(range,needed) { uint64_t rng = (range) ; needed = 1; while (rng >>= 1) needed++ ; }
 
 // 32 and 64 bit left aligned masks
 #define LMASK32(nbits)  ((~0)   << (32-nbits))
 #define LMASK64(nbits)  ((~0l)  << (64-nbits))
 
-// 32 and 64 bit left aligned masks
+// 32 and 64 bit right aligned masks
 #define RMASK32(nbits)  (~((~0)  << nbits))
 #define RMASK64(nbits)  (~((~0l) << nbits))
 
@@ -48,7 +50,7 @@ STATIC inline uint32_t BitsNeeded(int32_t what){
     double   f;
     uint64_t u;
   } t ;
-  if(what == 0) return 1 ;
+  if(what == 0) return 0 ;
 
   t.f = what ;
   it = ((t.u >> 52) & 0x7FF) - 1023 + 1 ; // exponent - bias + 1
@@ -56,13 +58,62 @@ STATIC inline uint32_t BitsNeeded(int32_t what){
   return (it > 32) ? 32 : it ;
 }
 
+STATIC inline uint32_t BitsNeeded24(int32_t what){
+  int it ;
+  union{
+    float    f;
+    uint32_t u;
+  } t ;
+  if(what == 0) return 0 ;
+
+  t.f = what ;
+  it = ((t.u >> 23) & 0xFF) - 127 + 1 ; // exponent - bias + 1
+  it += (t.u >> 31) ;
+  return (it > 32) ? 32 : it ;
+}
+
+// add to number of bits needed distribution
+// what : integer array of dimension n
+// pop  ; integer array of dimension 33 containing the number of bits needed distribution
+// n    : number of signed integers in what
 STATIC inline void BitPop(int32_t *what, uint32_t *pop, int n){
   int i;
   for(i=0 ; i<n ; i++) {
-    int nbits = BitsNeeded(what[i]) ;
-    pop[nbits]++ ;
+    int nbits = BitsNeeded(what[i]) ;  // number of bits needed for this signed integer
+    pop[nbits]++ ;                     // bump count for this number of bits
 //     pop[0]++ ;
   }
 }
 
+#endif
+
+#else
+  interface
+    function BitsNeeded(what) result(nbits) bind(C,name='BitsNeeded')
+      import C_INT32_T
+      implicit none
+      integer(C_INT32_T), intent(IN), value :: what
+      integer(C_INT32_T) :: nbits
+    end function BitsNeeded
+    function BitsNeeded24(what) result(nbits) bind(C,name='BitsNeeded')
+      import C_INT32_T
+      implicit none
+      integer(C_INT32_T), intent(IN), value :: what
+      integer(C_INT32_T) :: nbits
+    end function BitsNeeded24
+    subroutine BitPop(what, pop, n) bind(C, name='BitPop')
+      import :: C_INT32_T
+      implicit none
+      integer(C_INT32_t), dimension(n), intent(IN) :: what
+      integer(C_INT32_t), dimension(34), intent(INOUT) :: pop
+      integer(C_INT32_t), intent(IN), value :: n
+    end subroutine BitPop
+    function BitEntropy(bitstream, npts, nbits, rshift) result(entropy) bind(C,name='BitEntropy')
+      import :: C_INT32_T, C_FLOAT
+      implicit none
+      integer(C_INT32_t), intent(IN), value :: npts, nbits, rshift
+      integer(C_INT32_t), dimension(*) :: bitstream
+      real(C_FLOAT) :: entropy
+    end function BitEntropy
+  end interface
 #endif
