de5e5f87264bbea8306f56187af972bbb0e3499b
M. Valin <mfvalin@gmail.com>
2022-10-27 15:38:15 -0400

    mode comments, added TIME_ONCE macros, more autonomous macros

--- PACK/misc_timers.h
+++ PACK/misc_timers.h
@@ -12,50 +12,68 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
  *
- * in the ARM V8 case, all the cycle counters use the same instruction
- * in the X86_64 case, various cycle counters and fences are used
+ * in the ARM V8 case, all the functions use the same instruction
+ * in the X86_64 case, various instructions and fences are used
  *
  * useful references :
  * https://sites.utexas.edu/jdm4372/2018/07/   John McCalpin's blog
  * https://github.com/jdmccalpin/low-overhead-timers
  */
-#if defined(IN_FORTRAN_CODE)
+#if defined(IN_FORTRAN_CODE) || defined(__GFORTRAN__)
+
 interface
-  function elapsed_us() result(t) bind(C,name='ElapsedUs')
+  function elapsed_us() result(t) bind(C,name='ElapsedUs')  ! elapsed microseconds
     import C_INT64_T
     implicit none
     integer(C_INT64_T) :: t
   end function elapsed_us
-  function elapsed_cycles() result(t) bind(C,name='ElapsedCycles')
+  function elapsed_cycles() result(t) bind(C,name='ElapsedCycles')  ! elapsed timer ticks
     import C_INT64_T
     implicit none
     integer(C_INT64_T) :: t
   end function elapsed_cycles
-  function cycles_counter_freq() result(t) bind(C,name='CyclesCounterFreq')
+  function cycles_counter_freq() result(t) bind(C,name='CyclesCounterFreq')  ! timer tick frequency
     import C_INT64_T
     implicit none
     integer(C_INT64_T) :: t
   end function cycles_counter_freq
 end interface
+
 #else
 
-// tmin, tmax, tavg can be floats or doubles, niter is expected to be an integer
+// protect against multiple include
+#if ! defined(TIME_LOOP_TOP)
+
+static double NaNoSeC = 0.0 ;
+
+// niter is expected to be an integer scalar variable
 //
-// timing loop top part
+// TIME_LOOP_TOP : timing loop top part
 // niter : number of iterations
+// to is used to compensate elapsed_cycles() overhead ( 7/8 of elapsed_cycles() time)
+//
+// TIME_ONCE_TOP does not need niter
 #define TIME_LOOP_TOP(niter) \
 { uint64_t t, to , mint = ~0, maxt = 0, avgt = 0.0 ; int iter = niter ; \
-  for(j=0 ; j < iter ; j++) { to = elapsed_cycles() ; t = elapsed_cycles() ; to = t - to ;
+  if(NaNoSeC == 0) NaNoSeC = 1.0E+9f / cycles_counter_freq() ; \
+  to = elapsed_cycles() ; t = elapsed_cycles() ; to = t - to ; to = to - (to >> 3) ; \
+  for(j=0 ; j < iter ; j++) { t = elapsed_cycles() ;
+#define TIME_ONCE_TOP \
+{ uint64_t t, to ; double NaNoSeC = 1.0E+9f / cycles_counter_freq() ; \
+  to = elapsed_cycles() ; t = elapsed_cycles() ; to = t - to ; to = to - (to >> 3) ;
 //
 // code to be timed in a loop goes between TIME_LOOP_TOP and TIME_LOOP_BOT
+// tmin, tmax, tavg can be floats or doubles (scalar variables)
 //
-// timing loop bottom part
+// TIME_LOOP_BOT : timing loop bottom part
 // tmin  : best timing in cycles
 // tmax  : worst timing in cycles
 // tavg  : average timing in cycles
 // npts  : number of points processed
 // buf   : buffer to receive diagnostic text
 // bufsiz: size of buf
+//
+// TIME_ONCE_BOT does not need tmin, tmax, tavg
 #define TIME_LOOP_BOT(tmin, tmax, tavg, npts, buf, bufsiz) \
     t = elapsed_cycles() -t -to ; \
     avgt += t ; mint = (t < mint) ? t : mint ; maxt = (t > maxt) ? t : maxt ; \
@@ -63,10 +81,19 @@ end interface
   tmin = mint ; tmax = maxt ; tavg = avgt/iter ; \
   if(npts > 0 && buf != NULL) \
     snprintf(buf, (size_t)bufsiz, " npts = %d, niter = %d,  ns= %6.0f (%6.0f), %6.2f ns/pt", \
-             npts, iter, tmin*nano, tavg*nano, tavg*nano/npts) ; \
+             npts, iter, tmin*NaNoSeC, tavg*NaNoSeC, tavg*NaNoSeC/npts) ; \
+}
+#define TIME_ONCE_BOT(npts, buf, bufsiz) \
+    t = elapsed_cycles() -t -to ; \
+    if(npts > 0 && buf != NULL) \
+    snprintf(buf, (size_t)bufsiz, " npts = %d,  ns= %6.0f, %6.2f ns/pt", \
+             npts, t*NaNoSeC, t*NaNoSeC/npts) ; \
 }
 
-// time a piece of code in a loop, get min, max, average time
+// tmin, tmax, tavg can be floats or doubles (scalar variables)
+// niter is expected to be an integer scalar variable
+//
+// TIME_LOOP : time a piece of code in a loop, get min, max, average time
 // tmin  : best timing in cycles
 // tmax  : worst timing in cycles
 // tavg  : average timing in cycles
@@ -75,10 +102,16 @@ end interface
 // TimedCode : code to be timed n (>=1) statement(s)
 // buf   : buffer to receive diagnostic text
 // bufsiz: size of buf
+//
+// TIME_ONCE does not need tmin, tmax, tavg, niter
 #define TIME_LOOP(tmin, tmax, tavg, niter, npts, buf, bufsiz, TimedCode) \
   TIME_LOOP_TOP(niter) ; \
   TimedCode ; \
   TIME_LOOP_BOT(tmin, tmax, tavg, npts, buf, bufsiz) ;
+#define TIME_ONCE(npts, buf, bufsiz, TimedCode) \
+  TIME_ONCE_TOP ; \
+  TimedCode ; \
+  TIME_ONCE_BOT(npts, buf, bufsiz) ;
 
 #if ! defined(MISC_TIMERS)
 #define MISC_TIMERS
@@ -87,6 +120,8 @@ end interface
 #include <sys/time.h>
 #include <stddef.h>
 
+// elapsed microseconds of wall clock time
+// an effective resolution of O(microsecond) is assumed for gettimeofday
 static inline uint64_t elapsed_us(void){
   struct timeval t ;
   uint64_t elapsed ;
@@ -97,7 +132,7 @@ static inline uint64_t elapsed_us(void){
   return elapsed ;
 }
 
-// NO serializing, NO fencing
+// elapsed timer ticks, NO serializing, NO fencing
 static inline uint64_t elapsed_cycles_fast(void) {
 #if defined(__x86_64__)
   uint64_t lo, hi ;
@@ -112,7 +147,7 @@ static inline uint64_t elapsed_cycles_fast(void) {
 #endif
 }
 
-// WITH serializing, NO fencing
+// elapsed timer ticks, WITH serializing, NO fencing
 static inline uint64_t elapsed_cycles_nofence(void) {
 #if defined(__x86_64__)
   uint64_t lo, hi, misc ;
@@ -127,7 +162,7 @@ static inline uint64_t elapsed_cycles_nofence(void) {
 #endif
 }
 
-// WITH serializing, WITH memory fencing after
+// elapsed timer ticks, WITH serializing, WITH memory fencing after
 static inline uint64_t elapsed_cycles_fenced(void) {
 #if defined(__x86_64__)
   uint64_t lo, hi, misc ;
@@ -143,8 +178,8 @@ static inline uint64_t elapsed_cycles_fenced(void) {
 #endif
 }
 
-// NO serializing, LOCAL fencing before
-// default form
+// elapsed timer ticks, NO serializing, LOCAL fencing before
+// default version
 static inline uint64_t elapsed_cycles(void) {
 #if defined(__x86_64__)
   uint64_t lo, hi ;
@@ -160,6 +195,7 @@ static inline uint64_t elapsed_cycles(void) {
 #endif
 }
 
+// determine the timer tick frequency (in Hz)
 static inline uint64_t cycles_counter_freq(void){
   static uint64_t timerfreq = 0;
   uint64_t t1, t2, tc1, tc2 ;
@@ -167,10 +203,12 @@ static inline uint64_t cycles_counter_freq(void){
   if(timerfreq != 0) return timerfreq ;
 
 #if defined(__aarch64__)
+  // we are lucky, ther is an instruction that does it
   asm volatile("isb ; mrs %0, cntfrq_el0" : "=r"(timerfreq));
   return timerfreq ;
 #endif
-
+  // loop using gettimeofday for about 1 millisecond
+  // assumes O(microsecond) effective resolution for gettimeofday
   t2 = t1 = elapsed_us() ;
   tc1 = elapsed_cycles() ;
   while(t2-t1 < 1000){    // 1 millisecond
@@ -181,9 +219,12 @@ static inline uint64_t cycles_counter_freq(void){
   return timerfreq ;
 }
 
+// entry points used for Fortran interface (code in misc_timers.c)
 uint64_t ElapsedUs(void);
 uint64_t ElapsedCycles(void);
 uint64_t CyclesCounterFreq(void);
+
+#endif
 
 #endif
 
